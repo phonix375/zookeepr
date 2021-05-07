6b4c0093944103e3fd255700b3d417c15aed243e
Alexy Kotliar <kotliar.alex@gmail.com>
2021-05-06 23:50:28 -0400

    add to heroku test 4

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/api/animals', (req, res) =>{
     res.json(results);
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 
   });
