89a6d3387d93b816882029dd9e1476cc2a6bf5e3
Alexy Kotliar <kotliar.alex@gmail.com>
2021-05-07 16:02:41 -0400

    finished MVP

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 const {animals} = require('./data/animals')
 const PORT = process.env.PORT || 3001;
 
-
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 
 function filterByQuery(query, animalsarray){
@@ -44,6 +47,16 @@ app.get('/api/animals', (req, res) =>{
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
