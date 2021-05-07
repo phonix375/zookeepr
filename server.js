b656163b58cf8cbfeb0bb01b4de8eb06d5fea08f
Alexy Kotliar <kotliar.alex@gmail.com>
2021-05-06 23:38:28 -0400

    add to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const {animals} = require('./data/animals')
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 function filterByQuery(query, animalsarray){
     let personalityTraitsArray = [];
