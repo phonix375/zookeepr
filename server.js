0cfcd9eb583c5d5b0627b2d9d24cae6361a8bade
Alexy Kotliar <kotliar.alex@gmail.com>
2021-05-06 23:21:56 -0400

    add to heroku

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const {animals} = require('./data/animals')
+
+function filterByQuery(query, animalsarray){
+    let personalityTraitsArray = [];
+    let filteredResults =animalsarray;
+    console.log(query);
+        if(query.personalityTraits){
+            if(typeof query.personalityTraits === 'string'){
+                personalityTraitsArray = [query.personalityTraits];
+            }else{
+                personalityTraitsArray = query.personalityTraits;
+            }
+            personalityTraitsArray.forEach(trait => {
+                filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1
+                );
+            });
+        }
+     
+        if(query.diet){
+            filteredResults = filteredResults.filter(animal => animal.diet === query.diet)
+        }
+        if (query.species) {
+            filteredResults = filteredResults.filter(animal => animal.species === query.species)
+        }
+        if (query.name) {
+            console.log('going to the if');
+            filteredResults = filteredResults.filter(animal => animal.name === query.name)
+            console.log(filteredResults);
+        }
+        return filteredResults;
+    }
+
+
+app.get('/api/animals', (req, res) =>{
+    let results = animals;
+    if(req.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+
+  });
