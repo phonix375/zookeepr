8b8a850e00215e7c21e6abd7794ea45bcfcd96ca
Alexy Kotliar <kotliar.alex@gmail.com>
2021-05-07 23:50:11 -0400

    adding post rout

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+
+
 const app = express();
+//pars ubcoming string or array data
+app.use(express.urlencoded({extended: true}));
+//pars incoming JSON data
+app.use(express.json());
 const {animals} = require('./data/animals')
 const PORT = process.env.PORT || 3001;
 
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
-  }
+  };
+
+function createNewAnimal(body, animalsArray){
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals:animalsArray})
+    )
+    
+    return animal;
+}
 
 
 function filterByQuery(query, animalsarray){
@@ -57,6 +77,14 @@ app.get('/api/animals/:id', (req, res) => {
     }
   });
 
+  app.post('/api/animals', (req, res) => {
+      req.body.id = animals.length.toString();
+    // req.body is where our incoming content will be
+    const animal = createNewAnimal(req.body, animals);
+
+
+    res.json(animal);
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
