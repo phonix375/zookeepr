19d61b3a77f629cc87d632c4fda48dd89c345afc
Alexy Kotliar <kotliar.alex@gmail.com>
2021-05-08 13:02:20 -0400

    finished adding the front end

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 //pars incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 const {animals} = require('./data/animals')
 const PORT = process.env.PORT || 3001;
 
@@ -74,6 +75,18 @@ function filterByQuery(query, animalsarray){
         return filteredResults;
     }
 
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
 
 app.get('/api/animals', (req, res) =>{
     let results = animals;
@@ -94,6 +107,7 @@ app.get('/api/animals/:id', (req, res) => {
 
   app.post('/api/animals', (req, res) => {
         req.body.id = animals.length.toString();
+        console.log('Im here');
 
         //if any data in req.body is incorrect, return 400 error back
         if(!validateAnimal(req.body)){
@@ -103,6 +117,10 @@ app.get('/api/animals/:id', (req, res) => {
             res.json(animal);
         }
   });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
